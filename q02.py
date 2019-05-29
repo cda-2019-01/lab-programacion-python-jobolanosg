3ee867c0e336fcec53f899ff86fc0af2cc08a8dd
jobolanosg <50249777+jobolanosg@users.noreply.github.com>
2019-05-28 21:28:05 -0500

    Update q02.py

--- q02.py
+++ q02.py
@@ -8,3 +8,14 @@
 ## D,6
 ## E,14
 ##
+file = open('data.csv', 'r').readlines()
+file = [row.replace('\t', ' ') for row in file]
+file = [row.replace('\n', ' ') for row in file]
+file = [row.split(',') for row in file]
+file = [row[0].split(' ') for row in file[0:]]
+temp = []
+[temp.append(row[0]) for row in file]
+letters = sorted(list(set(temp)))
+temp=[(w, temp.count(w)) for w in letters]
+for row in temp:
+    print(row[0],row[1], sep=",")
