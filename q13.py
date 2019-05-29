5a6f2dd5f672751b131209e0b25cf58260c6b4ed
jobolanosg <50249777+jobolanosg@users.noreply.github.com>
2019-05-28 21:36:07 -0500

    Update q13.py

--- q13.py
+++ q13.py
@@ -10,3 +10,25 @@
 ## E,11
 ## E,16
 ##
+file = open('data.csv', 'r').readlines()
+file = [row.replace('\t', ' ') for row in file]
+file = [row.replace('\n', '') for row in file]
+file = [row.split(' ') for row in file]
+
+temp = []
+i = 0
+for f in file:
+	temp.append([])
+	for a in f:
+		b = a.split(',')
+		if(len(b) == 1):
+			temp[i].append(b[0])
+		else:
+			temp[i].append(b)
+	i += 1
+
+for x in temp:
+	aux = 0
+	for a in x[4]:
+		aux += int(a.split(':')[1])
+	print(x[0] + ',' + str(aux))
