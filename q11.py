245e59570c07bec8d9baa98e928862164a3252b4
jobolanosg <50249777+jobolanosg@users.noreply.github.com>
2019-05-28 21:34:43 -0500

    Update q11.py

--- q11.py
+++ q11.py
@@ -11,3 +11,22 @@
 ## E,2,3
 ## E,3,3
 ##
+file = open('data.csv', 'r').readlines()
+file = [row.replace('\t', ' ') for row in file]
+file = [row.replace('\n', '') for row in file]
+file = [row.split(' ') for row in file]
+
+temp = []
+i = 0
+for f in file:
+	temp.append([])
+	for a in f:
+		b = a.split(',')
+		if(len(b) == 1):
+			temp[i].append(b[0])
+		else:
+			temp[i].append(b)
+	i += 1
+
+for x in temp:
+	print(x[0] + ',' + str(len(x[3])) + ',' + str(len(x[4])))
