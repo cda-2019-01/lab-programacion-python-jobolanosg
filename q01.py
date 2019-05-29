9c8c2b8e449e041e1bf25ea5495619b411cbbf25
jobolanosg <50249777+jobolanosg@users.noreply.github.com>
2019-05-28 21:27:09 -0500

    Update q01.py

--- q01.py
+++ q01.py
@@ -1,3 +1,13 @@
 ##
 ## Imprima la suma de la segunda columna.
 ##
+## Limpieza de la data
+file = open('data.csv', 'r').readlines()
+file = [row.replace('\t', ' ') for row in file]
+file = [row.replace('\n', ' ') for row in file]
+file = [row.split(',') for row in file]
+file = [row[0].split(' ') for row in file[0:]]
+## Realizar la suma e imprimir
+temp = 0
+temp = [int(x[1]) for x in file]
+print(sum(temp))
