5dc25ed900b7dcbcb4fd42c664993a53d9b290ef
jobolanosg <50249777+jobolanosg@users.noreply.github.com>
2019-05-28 21:34:01 -0500

    Update q10.py

--- q10.py
+++ q10.py
@@ -15,3 +15,34 @@
 ## jjj,18
 ##
 ##
+file = open('data.csv', 'r').readlines()
+file = [row.replace('\t', ' ') for row in file]
+file = [row.replace('\n', '') for row in file]
+file = [row.split(' ') for row in file]
+
+temp1 = []
+i = 0
+for f in file:
+	temp1.append([])
+	for a in f:
+		b = a.split(',')
+		if(len(b) == 1):
+			temp1[i].append(b[0])
+		else:
+			temp1[i].append(b)
+	i += 1
+
+temp = {}
+
+for x in temp1:
+	for y in x[4]:
+		aux = y.split(':')
+		temp[aux[0]] = 0
+
+for x in temp1:
+	for y in x[4]:
+		aux = y.split(':')
+		temp[aux[0]] += 1
+
+for z in sorted(temp):
+	print(z + ',' + str(temp[z]))
