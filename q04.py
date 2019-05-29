8c0da10c9cc3c684aaaf032d879ef723f791e6ec
jobolanosg <50249777+jobolanosg@users.noreply.github.com>
2019-05-28 21:29:32 -0500

    Update q04.py

--- q04.py
+++ q04.py
@@ -14,3 +14,15 @@
 ## 11,2
 ## 12,3
 ##
+file = open('data.csv', 'r').readlines()
+file = [row.replace('\t', ' ') for row in file]
+file = [row.replace('\n', ' ') for row in file]
+file = [row.split(',') for row in file]
+file = [row[0].split(' ') for row in file[0:]]
+temp = {}
+for x in file:
+	temp[(x[2].split('-')[1])] = 0
+for x in file:
+	temp[(x[2].split('-')[1])] = temp[(x[2].split('-')[1])] + 1
+for y in sorted(temp.keys()):  
+     print(y + ',' + str(temp[y]))
