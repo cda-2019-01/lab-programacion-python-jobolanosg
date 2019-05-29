3595286925dcfaaaa6d827311835190e2c07eef3
jobolanosg <50249777+jobolanosg@users.noreply.github.com>
2019-05-28 21:30:20 -0500

    Update q05.py

--- q05.py
+++ q05.py
@@ -7,3 +7,15 @@
 ## D,7,1
 ## E,9,1
 ##
+file = open('data.csv', 'r').readlines()
+file = [row.replace('\t', ' ') for row in file]
+file = [row.replace('\n', ' ') for row in file]
+file = [row.split(',') for row in file]
+file = [row[0].split(' ') for row in file[0:]]
+temp = {}
+for x in file:
+	temp[x[0]] = []
+for x in file:
+	temp[x[0]].append(int(x[1]))
+for y in sorted(temp.keys()):  
+     print(y + ',' + str(max(temp[y])) + ',' + str(min(temp[y])))
